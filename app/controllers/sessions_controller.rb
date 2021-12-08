7ffe5af9430ae96dc1b9da6e592408470919299d
Lispooh13 <Riaya1203@gmail.com>
2021-12-08 08:52:23 +0000

    ログイン機能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
   
   def new
   end
@@ -6,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      login_url
+      log_in user
       redirect_to root_url
     else
       render 'new'
